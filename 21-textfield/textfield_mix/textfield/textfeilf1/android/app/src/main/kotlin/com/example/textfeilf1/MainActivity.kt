bae5d669dc8da1ab79b1284d0dcfa98b1350537b
divyahadiya <hadiyadivu125@gmail.com>
2024-04-04 17:35:52 +0530

    done

--- 21-textfield/textfield_mix/textfield/textfeilf1/android/app/src/main/kotlin/com/example/textfeilf1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.textfeilf1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- 21-textfield/textformfeild2/android/app/src/main/kotlin/com/example/textformfeild2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.textformfeild2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
