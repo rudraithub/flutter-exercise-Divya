1b13536d1d219b24cd7d21a8c154ea6522c9acc1
divyahadiya <hadiyadivu125@gmail.com>
2024-04-04 17:28:12 +0530

    done

--- 20-text/text/lib/main.dart
+++ 20-text/text/lib/main.dart
@@ -1,8 +1,8 @@
 
 
 
+import 'package:dart1/2001.dart';
 import 'package:flutter/material.dart';
-import 'package:text1/2002.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:MyText1 (),
+      home:MyText (),
     );
   }
 }
